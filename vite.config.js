ae89806c7f1f66d4d1f628f0111c3d3a6d591a2b
milagros <milagros.ch.contractor@bbva.com>
2025-04-07 18:20:26 +0200

    Configuración base para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/curso-lit/", // añadido para el despliegue en GitHub Pages
   test: {
     environment: "jsdom", // Simula el DOM en los tests
   },
